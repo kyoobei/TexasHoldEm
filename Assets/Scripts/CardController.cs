87a470057ab6f4fb81c386e174689ac27831382f
Gelo <kyoobei01@gmail.com>
2021-01-13 02:07:01 +0800

    modified Sample scene

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class CardController : MonoBehaviour, IController
 {
+    private const int NUM_PLAYER_CARDS = 2;
+    private const int NUM_MIDDLE_CARDS = 5;
     [SerializeField] private Pooler m_cardPooler = null;
+    [SerializeField] List<Transform> m_playerPositions = new List<Transform>();
+    [SerializeField] Transform m_middlePosition = null;
     public void StartController()
     {
         Debug.Log("working as shit");
+        //NOTE create own scripts for card handler (players and middle area)
     }
 }
