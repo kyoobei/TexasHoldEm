ff8c64faf50ea01b362060160d5e5d654bb3951c
Gelo <kyoobei01@gmail.com>
2021-01-14 11:56:06 +0800

    modified CardBehaviour.cs and prefab_card

--- Assets/Scripts/CardBehaviour.cs
+++ Assets/Scripts/CardBehaviour.cs
@@ -4,23 +4,21 @@ public class CardBehaviour : MonoBehaviour
 {
     private const float MIN_DISTANCE = 0.1f;
     [SerializeField] private float movementSpeed = 0.0f;
+    [SerializeField] private float rotationSpeed = 0.0f;
     [SerializeField] private CardDisplayBehaviour m_cardDisplayBehaviour = null;
     private Vector3 m_targetPosition = Vector3.zero;
     private bool m_shouldMove = false;
+    private bool m_shouldRotate = false;
     private void Update()
     {
-        if(!m_shouldMove)
-            return;
-
-        float distanceToTarget = Vector3.Distance(transform.position, m_targetPosition);
-        if(distanceToTarget > MIN_DISTANCE)
+        if(m_shouldMove)
         {
-            transform.position = Vector3.MoveTowards(transform.position, m_targetPosition, movementSpeed * Time.deltaTime);
+            MoveTowardsTarget();
         }
-        else if(distanceToTarget <= MIN_DISTANCE)
+        if(m_shouldRotate)
         {
-            m_shouldMove = false;
-        }
+            RotateCardToFaceUp();
+        }  
     }
     public void UpdateDisplay(Card card)
     {
@@ -34,6 +32,26 @@ public class CardBehaviour : MonoBehaviour
     public void ResetBehaviour()
     {
         m_targetPosition = Vector3.zero;
+        m_shouldRotate = false;
         m_shouldMove = false;
+    }
+    private void MoveTowardsTarget()
+    {
+        float distanceToTarget = Vector3.Distance(transform.position, m_targetPosition);
+        if(distanceToTarget > MIN_DISTANCE)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, m_targetPosition, movementSpeed * Time.deltaTime);
+        }
+        else if(distanceToTarget <= MIN_DISTANCE)
+        {
+            m_shouldMove = false;
+            m_shouldRotate = true;
+        }
+    }
+    private void RotateCardToFaceUp()
+    {
+        Quaternion cardFaceUpRotation = Quaternion.Euler(0,0f,180f);
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+            cardFaceUpRotation, rotationSpeed * Time.deltaTime);
     }
 }
