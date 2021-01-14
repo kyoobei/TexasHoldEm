622e926738a9e06994c7e17c3faf61ac6d540650
Gelo <kyoobei01@gmail.com>
2021-01-14 11:32:28 +0800

    Modified Card Deal

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -1,14 +1,11 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 public class CardController : MonoBehaviour, IController
 {
     private const int TOTAL_CARDS = 9;
-    //private Action OnGet
-    [SerializeField] private Pooler m_cardPooler = null;
-    [SerializeField] private List<Card> m_currentCardsAcquired;
-    //[SerializeField] private List<CardDeal> m_
+    [SerializeField] private List<CardDeal> m_cardDealersList = new List<CardDeal>();
+    [SerializeField] private CardDeal m_middleTable = null;
+    // private List<Card> m_currentCardsAcquired = new List<Card>();
     private Deck m_deck = new Deck();
     private void Start()
     {
@@ -24,18 +21,19 @@ public class CardController : MonoBehaviour, IController
     }
     private void FinishedShuffling()
     {
-        m_currentCardsAcquired.Clear();
-        m_currentCardsAcquired = new List<Card>(m_deck.GetCardsFromDeck(TOTAL_CARDS));
-        //UpdatePooledCards();
+        // m_currentCardsAcquired.Clear();
+        // m_currentCardsAcquired = new List<Card>(m_deck.GetCardsFromDeck(TOTAL_CARDS));
+        UpdateCardDealers();
     }
-    // private void UpdatePooledCards()
-    // {
-    //     for(int i = 0; i < TOTAL_CARDS; i++)
-    //     {
-    //         GameObject cardObject = m_cardPooler.GetClone();
-    //         CardBehaviour behaviour = cardObject.GetComponent<CardBehaviour>();
-    //         behaviour.ResetBehaviour();
-    //         behaviour.UpdateDisplay(m_currentCardsAcquired[i]);
-    //     }
-    // }
+    private void UpdateCardDealers()
+    {
+        //for players
+        for(int i = 0; i < m_cardDealersList.Count; i++)
+        {
+            m_cardDealersList[i].SetCardValue
+                (m_deck.GetCardsFromDeck(m_cardDealersList[i].NumberOfCardsToDeal));
+        }
+        //for the middle table
+        m_middleTable.SetCardValue(m_deck.GetCardsFromDeck(m_middleTable.NumberOfCardsToDeal));
+    }
 }
--- Assets/Scripts/CardDeal.cs
+++ Assets/Scripts/CardDeal.cs
@@ -1,15 +1,38 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CardDeal : MonoBehaviour
 {
+    public int NumberOfCardsToDeal => m_numberOfCardsToDeal;
     [SerializeField] private Pooler m_cardPooler = null;
-    [SerializeField] protected Transform m_originPosition = null;
-    [SerializeField] protected int m_numberOfCardsToDeal = 0;
-
-    public void SetCardValue()
+    [SerializeField] private Transform m_originPosition = null;
+    [SerializeField] private int m_numberOfCardsToDeal = 0;
+    [SerializeField] private float m_paddingBetweenCards = 0.0f;
+    [SerializeField] private List<Card> m_currentCardsList;
+    public void SetCardValue(List<Card> cards)
     {
-
+        m_currentCardsList.Clear();
+        m_currentCardsList = new List<Card>(cards);
+        UpdatePooledCards();
+    }
+    private void UpdatePooledCards()
+    {
+        Vector3 startingPosition = m_originPosition.position;
+        for(int i = 0; i < m_numberOfCardsToDeal; i++)
+        {
+            GameObject cardObject = m_cardPooler.GetClone();
+            cardObject.SetActive(true);
+            CardBehaviour behaviour = cardObject.GetComponent<CardBehaviour>();
+            behaviour.ResetBehaviour();
+            behaviour.UpdateDisplay(m_currentCardsList[i]);
+            behaviour.MoveCard(startingPosition);
+            //update position
+            startingPosition = new Vector3
+            (
+                startingPosition.x + m_paddingBetweenCards,
+                startingPosition.y,
+                startingPosition.z
+            );
+        }
     }
 }
