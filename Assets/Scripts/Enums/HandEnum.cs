12e293716d575865a87f5123ca77621a38d4f6a0
Gelo <kyoobei01@gmail.com>
2021-01-14 14:51:19 +0800

    Updated Scene

---
+++ Assets/Scripts/Enums/HandEnum.cs
@@ -0,0 +1,13 @@
+public enum Hand
+{
+    HIGH_CARD,
+    ONE_PAIR,
+    TWO_PAIR,
+    THREE_OF_A_KIND,
+    STRAIGHT,
+    FULL_HOUSE,
+    FLUSH,
+    FOUR_OF_A_KIND,
+    STRAIGHT_FLUSH,
+    ROYAL_FLUSH
+}
