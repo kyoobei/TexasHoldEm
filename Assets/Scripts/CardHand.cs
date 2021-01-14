46f8cbf81dd313edbb883d0f22654bd85ea2e470
Gelo <kyoobei01@gmail.com>
2021-01-14 23:54:46 +0800

    modified CardHand.cs

--- Assets/Scripts/CardHand.cs
+++ Assets/Scripts/CardHand.cs
@@ -1,50 +1,50 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 public class CardHand
 {    
-     public List<Card> m_combineCards;
-    private List<Card> winningCardList;
+    public List<Card> m_combineCards;
+    private List<Card> winningCardList = new List<Card>();
     private List<Card> m_originalCards;
     private List<Card> m_otherCards;
-    // public void CurrentHand(List<Card> currentHand, List<Card> cardToCompare)
-    // {
-    //     for(int i = 0; i < currentHand.Count; i++)
-    //     {
-    //         //adding of each card at hand to the middle card area or something
-    //         List<Card> temporaryCardList = new List<Card>();
-    //         temporaryCardList.Add(currentHand[i]);
-    //         cardToCompare.ForEach(cards => temporaryCardList.Add(cards));
-
-            
-    //     }
-    // }
     public /*Hand*/void GetHandType(List<Card> originalHand, List<Card> otherHand)
     {
-         m_combineCards = new List<Card>();
+        m_combineCards = new List<Card>();
         m_originalCards = new List<Card>(originalHand);
         m_otherCards = new List<Card>(otherHand);
         originalHand.ForEach(hand => m_combineCards.Add(hand));
         otherHand.ForEach(hand => m_combineCards.Add(hand));
         m_combineCards.Sort((handA, handB) => handA.Value.CompareTo(handB.Value));
 
-        Debug.Log("Is straight: " + IsStraight());
-        // if(IsStraight())
+        if(IsStraight())
+        {
+            Debug.Log("is a straight");
+            if(IsFlush())
+            {
+                Debug.Log("This is a straight flush");
+                if(winningCardList[winningCardList.Count - 1].Value == (int)HighCard.Ace)
+                {
+                    Debug.Log("this is a royal flush");
+                }
+            }
+        }
+        if(IsFlush())
+        {
+            //this will display since its not yet a method that returns a Hand enum
+            Debug.Log("this is just a flush");
+        }
+        // if(winningCardList.Count > 0)
         // {
-        //     return Hand.STRAIGHT;
+        //     for(int i = 0; i < winningCardList.Count; i++)
+        //     {
+        //         Debug.Log("this is the winning: " + winningCardList[i].Suit + " " + winningCardList[i].Value);
+        //     }
         // }
-        // // if(IsStraight())
-        // // {
-        // //     if(IsFlush())
-        // //     {
-        // //         return Hand.STRAIGHT_FLUSH;
-        // //     }
-        // //     return Hand.STRAIGHT;
-        // // }
-        // // if(IsFlush())
-        // // {
-        // //     return Hand.FLUSH;
-        // // }
-
+        // else
+        // {
+        //     Debug.Log("no winning list");
+        // }
+        
         // return Hand.HIGH_CARD;
     }
     public bool IsHandEqual(List<Card> cardA, List<Card> cardB)
@@ -57,6 +57,7 @@ public class CardHand
     }
     private bool IsStraight()
     {
+        //TODO: if there will be same number but different suit there will be a problem or bug
         List<Card> straightCard = new List<Card>();
         for(int i = 0; i < m_combineCards.Count; i++)
         {
@@ -64,14 +65,17 @@ public class CardHand
             {
                 if((m_combineCards[i].Value + 1) == m_combineCards[i + 1].Value)
                 {
-                    if(!straightCard.Contains(m_combineCards[i]))
+                    if(straightCard.Count < 5)
                     {
-                        straightCard.Add(m_combineCards[i]);
+                        if(!straightCard.Contains(m_combineCards[i]))
+                        {
+                            straightCard.Add(m_combineCards[i]);
+                        }
+                        if(!straightCard.Contains(m_combineCards[i + 1]))
+                        {
+                            straightCard.Add(m_combineCards[i + 1]); 
+                        }                 
                     }
-                    if(!straightCard.Contains(m_combineCards[i + 1]))
-                    {
-                        straightCard.Add(m_combineCards[i + 1]); 
-                    }                 
                 }
                 else if((m_combineCards[i].Value + 1) != m_combineCards[i + 1].Value)
                 {
@@ -95,6 +99,22 @@ public class CardHand
     }
     private bool IsFlush()
     {
+        if(winningCardList.Count > 0)
+        {
+            return winningCardList.GroupBy(card => card.Suit).Count() == 1;
+        }
+        else if(winningCardList.Count <= 0)
+        {
+            foreach(var groups in m_combineCards.GroupBy(card => card.Suit))
+            {
+                if(groups.Count() == 5 && (groups.Contains(m_originalCards[0]) ||
+                    groups.Contains(m_originalCards[1])))
+                {
+                    winningCardList = new List<Card>(groups);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
